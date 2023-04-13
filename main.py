b729473549d99718f89f3cdcdea3c7c18735251e
Keerthana2344 <129586268+Keerthana2344@users.noreply.github.com>
2023-04-13 09:12:57 +0000

    written a program for greeting

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello vidya")
+name=input("Enter your name: ")
+time=float(input("Enter the current time: "))
+
+if(time<=12):
+  print("Good morning ",name)
+elif(time<=16):
+  print("Good afternoon ",name)
+elif(time<=20):
+  print("Good evening ",name)
+else:
+  print("Good night")
